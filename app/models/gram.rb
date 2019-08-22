de61f418c8e7364b3973a373e3978d3bf328f03c
Jackson Scott <scottja23@gmail.com>
2019-08-22 00:14:17 +0000

    added comments to grams

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,7 +1,8 @@
 class Gram < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :message, presence: true
-  
+  validates :picture, presence: true
+  has_many :comments
 
   belongs_to :user
 end
